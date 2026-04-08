801e1c58338d6af1479f643cd00c9fec75036fa9
aldeirosultan <sultan.aldeiro@gmail.com>
2026-04-07 18:12:35 -0700

    fixed adminSeed

--- backend/seed/adminSeed.js
+++ backend/seed/adminSeed.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv')
 const dns = require('dns')
 
 dns.setServers(['8.8.8.8', '8.8.4.4'])
-dotenv.config({ path: '../.env' })
+dotenv.config()
 
 const User = require('../models/User')
 
